c9af9b74c1ec873651ae481235f4b88c75da271b
Abhinav Mathur <raaaaj5000@yahoo.co.in>
2016-08-22 00:04:17 -0400

    semi styled products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821203217) do
+ActiveRecord::Schema.define(version: 20160821234643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20160821203217) do
     t.datetime "updated_at",                     null: false
     t.text     "product_images"
     t.string   "sub_category"
+    t.integer  "user_id"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
