f472f9ddd13e6a4b69116c4adad4d0f17b20bd6f
Norman Chan <slowmanchan@gmail.com>
2016-10-19 15:07:00 -0400

    changed files

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function(){
+	$('a[href="#search"]').on('click', function(event) {
+    event.preventDefault();
+		$('#search').addClass('open');
+		$('#search > form > input[type="search"]').focus();
+	});
+	$('#search, #search button.close').on('click keyup', function(event) {
+		if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+			$(this).removeClass('open');
+		}
+	});
+});
